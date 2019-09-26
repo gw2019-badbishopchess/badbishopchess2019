6da809552ec2f7bfdab1dbf0a9640324ee9902e6
alexgergar <alexandra.gergar@gmail.com>
2019-09-26 11:49:37 -0400

    created render method with x and y coords inside of game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,8 @@ class Game < ApplicationRecord
   belongs_to :user # Creating game to users association
 
   scope :available, -> { where state: "open" } #to help find which games are available
+
+  def render_piece(x_coord, y_coord)
+    "x: #{x_coord} y: #{y_coord}"
+  end
 end
