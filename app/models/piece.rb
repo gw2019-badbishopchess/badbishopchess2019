b0fdb6b99434a02ed829d41ff0e35bce6d003961
Shmulik <syraices@gmail.com>
2019-09-26 23:17:46 +0000

    is_obstructed? method finished

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,10 +2,18 @@ class Piece < ApplicationRecord
   has_one :user # Creating pieces to user association
   belongs_to :game # Creating pieces to game association
 
+  
+  # The occupied? method checks whether there is a piece a at (x, y)
+  # args = x coordinate and y coordinate of space
+  # returns true if occupied, false if unoccupied 
   def occupied?(x, y)
-    game.pieces.where(x_coordinate, y_coordinate).present?
+    game.pieces.where(x_coordinate: x, y_coordinate: y).present?
   end
 
+  
+  # The check_path method checks the piece's path
+  # args = piece coordinates and destination coordinates
+  # returns 'horizontal', 'vertical' or if slope, returns degree of slope
   def check_path(x1, y1, x2, y2)
     if y1 == y2 
       return 'horizontal'
@@ -16,9 +24,11 @@ class Piece < ApplicationRecord
     end
   end
 
-
-
-  def obstructed?(destination)
+  #The is_obstructed? method determines whether piece's path is obstructed
+  # args = array containing x and y coordinates of intended destination
+  # returns True if one or more spaces are occupied, otherwise False
+  # runtime error if not straight line
+  def is_obstructed?(destination)
     @game = game
 
     x1 = self.x_coordinate
@@ -57,12 +67,25 @@ class Piece < ApplicationRecord
       return false
     end
 
-    if @slope.abs == 1.0 && x1 > x2
+    if @slope.abs == 1.0 && x1 < x2
       (x1 - 1).downto(x2 + 1) do |x|
-        delta_y = x1 - x
+        delta_y = x - x1
         y = y2 > y1 ? y1 + delta_y : y1 - delta_y
         return true if occupied?(x, y)
       end
+    end
+
+    if @slope.abs == 1.0 && x1 > x2
+      x1 - 1.downto(x2 +1) do |x|
+        delta_y = x1 - x 
+        y = y2 > y1 ? y1 + delta_y : y1 - delta_y
+        return true if occupied?(x, y)
+      end
+    end
+
+    if @slope.abs != 1.0
+      fail 'path is not a straight line'
+    else return false
     end
   end
 
