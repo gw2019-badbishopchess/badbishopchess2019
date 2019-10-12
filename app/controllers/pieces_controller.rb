d5f5d83eacc8befaaf6c9627a917204c561dd5f7
alexgergar <alexandra.gergar@gmail.com>
2019-10-12 13:49:28 -0400

    Added @piece.update_attribes for x and y coords to update piece model and @piece.touch to update the updated_at colum which is needed for views. added logic to views to show a yellow bg to last updated (clicked on) piece

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,19 +4,22 @@ class PiecesController < ApplicationController
     @piece = Piece.find_by_id(params[:id])
     @game = @piece.game
     @pieces = @game.pieces
-    flash[:success] = "#{@piece.type}"
+    flash[:success] = "#{@piece.type} X: #{@piece.x_coordinate} Y: #{@piece.y_coordinate}"
     redirect_to game_path(@game)
   end 
 
-  def update(x_coordinate, y_coordinate)
+  def update
     @piece = Piece.find_by_id(params[:id])
+    @game = @piece.game #need this or game_path(@game) will throw an error
     x_coordinate = @piece.x_coordinate
     y_coordinate = @piece.y_coordinate
-    if @piece.valid_move?(x_coordinate, y_coordinate)
-      @piece.move_to!(x_coordinate, y_coordinate)
-    else
-      alert[:error] = "Not Valid Move"
-    end
+    # if @piece.valid_move?(x_coordinate, y_coordinate)
+    #   @piece.move_to!(x_coordinate, y_coordinate)
+    @piece.update_attributes(:x_coordinate => x_coordinate, :y_coordinate => y_coordinate)
+    @piece.touch # this will update the updated_at column need this to figure out what was updated last based on time (.last just gives last created)
+    # else
+    #   alert[:error] = "Not Valid Move"
+    # end
     redirect_to game_path(@game)
   end
 
