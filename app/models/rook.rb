03fee2cc9873c7f3299d69b84b426b8218f7f750
Shmulik <syraices@gmail.com>
2019-10-13 03:54:44 +0000

    valid move for rook

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,3 +1,9 @@
 class Rook < Piece
-  
+  def valid_move?(x_destination, y_destination)
+    if (x_coordinates == x_destination || y_coordinates == y_destination) && !is_obstructed?([x_destination, y_destination])
+        return true
+    else return false
+    end
+  end
+
 end
