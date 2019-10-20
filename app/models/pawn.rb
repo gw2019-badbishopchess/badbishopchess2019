3fc2af872d2af6f2b29f0b7e5afa0e0e3298b780
JP Horrigan <Horrigan.JP@outlook.com>
2019-10-20 19:46:19 +0000

    Added pawn_promotion functionality, v1

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -5,16 +5,31 @@ class Pawn < Piece
   #line 12 to see if the move is 1 space forward if white piece
   #line 14 to see if the move is 1 space forward if black piece
   def is_valid?(new_x_coord, new_y_coord)
-    if piece_move_count == 0 && x_distance(new_x_coord) == 0 && y_distance(new_y_coord) <= 2
+    x_dist = x_distance(new_x_coord)
+    y_dist = y_distance(new_y_coord)
+
+    if piece_move_count == 0 && x_dist == 0 && y_dist <= 2
       return true
     elsif diagonal_move?(new_x_coord, new_y_coord) && occupied?(new_x_coord, new_y_coord)
       return true
-    elsif color_white == true && x_distance(new_x_coord) == 0 && new_y_coord == self.y_coordinate + 1
+    elsif color_white == true && x_dist == 0 && y_dist == 1 && !occupied?(new_x_coord, new_y_coord) && new_y_coord.to_i >= self.y_coordinate
       return true
-    elsif color_white == false && x_distance(new_x_coord) == 0 && new_y_coord == self.y_coordinate - 1
+    elsif color_white == false && x_dist == 0 && y_dist == 1 && !occupied?(new_x_coord, new_y_coord) && new_y_coord.to_i <= self.y_coordinate
+      return true
+    elsif pawn_promption?
       return true
     else 
       return false
     end
   end
+
+# Existing Errors in pawn_promotion
+# Taking opponent piece right after promotion doesn't change piece image
+# Piece image doesn't change until after move
+  def pawn_promption?
+    pawn = game.pieces.where(type: "Pawn")
+    (self.y_coordinate == 8 && self.color_white == true) || (self.y_coordinate == 1 && self.color_white == false)
+    self.update_attributes(type: "Queen")
+  end
+
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -165,4 +165,6 @@ class Piece < ApplicationRecord
     end
   end
 
+   
+
 end
