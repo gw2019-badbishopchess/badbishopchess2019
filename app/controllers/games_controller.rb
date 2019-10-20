5faf65efb0fcf1d48dade326d5375c9217587e6a
Shmulik <syraices@gmail.com>
2019-10-20 17:12:58 +0000

    merge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -27,6 +27,19 @@ class GamesController < ApplicationController
   def join
     @game = Game.find_by_id(params[:id])
     redirect_to game_path(@game)
+    if current_user.id != @game.white_player_id
+      @game.update_attributes(game_params)
+       redirect_to game_path(@game)
+    else
+      flash[:alert] = "Error: You are already a player in this game!!" 
+      redirect_to games_path
+    end
+  end
+
+  def forfeit
+    @game = Game.find_by_id(params[:id])
+    @game.update_attributes(game_params)
+    redirect_to root_path
   end
 
   private
